375126b3d16ca63a1203d4a0749a256e5f7f51ab
ferhs343 <114626248+ferhs343@users.noreply.github.com>
2025-05-21 06:04:50 -0600

    Update DB_Tables.sh

--- DB_Tables.sh
+++ DB_Tables.sh
@@ -1,11 +1,12 @@
+
 #!/bin/bash
 
 function import_to_table() {
 
-    sqlite3 "$1" 2>> .logs.log <<EOF
+    sqlite3 ./Databases/$db_file 2>> .logs.log <<EOF
 .mode csv
 .separator ","
-.import data.csv $2
+.import data.csv $table
 EOF
 
     rm data.csv
@@ -20,17 +21,12 @@ function csv_convert() {
             printf("%s%s", $i, (i<NF ? "," : "\n"))
         }
     } 
-    ' $1 > data.csv
+    ' $sample_data > data.csv
 }
 
 function import_tcp_data() {
 
-    sample_data="./Results/.sample.data"
-    db_file="$2"
-    table="Flow_${1}_TCP"
-    csv_convert "$sample_data"
-
-    sqlite3 "$db_file" 2>> .logs.log <<EOF
+    sqlite3 ./Databases/"$db_file" 2>> .logs.log <<EOF
     CREATE TABLE IF NOT EXISTS $table (
         Session_Id INTEGER,
         Timestamp REAL,
@@ -50,18 +46,11 @@ function import_tcp_data() {
         Connection_Status TEXT
     );
 EOF
-
-    import_to_table "$db_file" "$table"
 }
 
 function import_udp_data() {
 
-    sample_data="./Results/.sample1.data"
-    db_file="$2"
-    table="Flow_${1}_UDP"
-    csv_convert "$sample_data"
-
-    sqlite3 "$db_file" 2>> .logs.log <<EOF
+    sqlite3 ./Databases/"$db_file" 2>> .logs.log <<EOF
     CREATE TABLE IF NOT EXISTS $table (
         Session_Id INTEGER,
         Timestamp REAL,
@@ -82,6 +71,37 @@ function import_udp_data() {
         ICMP_Status TEXT
     );
 EOF
+}
 
-    import_to_table "$db_file" "$table"
+function import_data() {
+
+    sample_data="./Results/${3}_${4}.parsed"
+    db_file="$2"
+    table="Flow_${1}_${3}"
+    csv_convert
+
+    if [ "$3" == "tcp" ]; then
+        import_tcp_data
+    elif [ "$3" == "udp" ]; then
+        import_udp_data
+    elif [ "$3" == "dns" ]; then
+        import_dns_data
+    elif [ "$3" == "http" ]; then
+        import_http_data
+    elif [ "$3" == "ssh" ]; then
+        import_ssh_data
+    elif [ "$3" == "smb2" ]; then
+        import_smb2_data
+    elif [ "$3" == "kerberos" ]; then
+        import_kerberos_data
+    elif [ "$3" == "ftp" ]; then
+        import_ftp_data
+    elif [ "$3" == "rpc" ]; then
+        import_rpc_data
+    elif [ "$3" == "dcerpc" ]; then
+        import_dcerpc_data
+    elif [ "$3" == "ntlm" ]; then
+        import_ntlm_data; fi
+
+    import_to_table
 }
