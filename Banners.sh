6d69a549778d0d4aa19ae228306f64f57047d5c2
ferhs343 <114626248+ferhs343@users.noreply.github.com>
2025-08-11 18:34:05 -0600

    Create Banners.sh

---
+++ Banners.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+function main_banner()
+{
+
+    echo ""
+    echo -e "    	 ${c}*${d}             /^--^\     /^--^\     /^--^\   "
+    echo -e "    ${c}*${d}                  \____/     \____/     \____/  "
+    echo -e "           ${c}*${d}          /      \   /      \   /      \  "
+    echo -e "                     ${d}|        | |        | |        |  "
+    echo -e "                      \__  __/   \__  __/   \__  __/  "
+    echo -e " ${f}|^|^|^|^|^|^|^|^|^|^|^|^${d}\ \^${f}|^|^|^${d}/ /${f}^|^|^|^|^${d}\ \^${f}|^|^|^|"
+    echo -e " | | | | | | | | | | | | |${d}\ \|${f} | |${d}/ /${f}| | | | | |${d}\ \|${f} | | |"
+    echo -e " ########################${d}/ /${f}######${d}\ \#${f}##########${d}/ /${f}####### "
+    echo -e " | | | | | | | | | | | | ${d}\/${f}| | | |${d} \/${f}| | | | | |${d}\/${f} | | | | "
+    echo -e " |_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_| ${d}"
+    echo -e "\n                           ${r}'The Denisse's Project' V.1.0.0   "
+    echo -e "                  By Luis F. Herrera - fer.hs343@gmail.com${d} \n"
+    echo " Welcome to 'The Denisse's Project', your traffic analyst "
+    echo " hunting for anomalies where others don't look.   "
+    echo " Happy Hunting!! :D"
+ 
+}
+
+function lf_banner()
+{
+
+    echo ""
+    echo -e " ${s}                                            . ."
+    echo -e "                               ,,   ,.@#(&*.;' "
+    echo -e "          ) ) )              .;#$% & ^^&${s}      "
+    echo -e "         ( ( (              ${r}..${s}      __  . .* ,${s}"           
+    echo -e "         ) ) )             ${d};. :   ${s}~#@#%(\" .,$ @${d}"
+    echo -e "      (${r}~~~~~~~~~${d})          ;${f}==${d}: ${s}.\"^ ';${d}     "
+    echo -e "       | ${f}POWER${d} |           ;. : ${r}..${d}  "
+    echo -e "       |       |           ;${f}==${d}:${f}.${d}  :      ${g}PCAP log factory: Gears${d}"
+    echo -e "       I     ${y} _._${d}          ;. :${f} ==${d}:      ${g}heat and packages; this${d} "
+    echo -e "       I    ${y}/'   \`\  ${d}      ;${f}==${d}:.  :      ${g}is how intelligence is ${d}"
+    echo -e "       I   ${y}|   N   |${d}       ;. :${f} ==${d}:      ${g}forged.${d}"
+    echo -e "       f   ${y}|${d}   |${o}~~~~~~~~~~~~~~${d}|   :  "
+    echo -e "     .'    ${y}|${d}   ||${o}~~~~~~~~${d}|    |${f} ==${d}:      ${o}TRRRR${d}... ${r}TSSSS${d}... ${c}CLANK!${d}"
+    echo -e "   /'______${y}|${d}___||__${y}###${d}___|____|.__: "
+}
+
+function lg_banner()
+{
+
+    echo ""
+    echo "         ⣠⣴⣾⣿⣿⣿⣿⣿⣿⣶⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀ "
+    echo "       ⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣄⠀⠀⠀⠀⠀                 "
+    echo -e "     ⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣆⠉⠉⢉⣿⣿⣿${o}⣷⣦⣄⡀${d}        PCAP log hunter"
+    echo -e "    ⠚⢛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿${o}⣿⣿⣿⣿⡄${g}                                           -----+  ${d}"
+    echo -e "    ⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿${o}⠿⠿⠿⣿⡇${d}       "Another day, another packet storm."       ${g}|${d}"
+    echo -e "   ⢀⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠁⠀⠀    ${o}⠈⠃${g}          =>${d} Unusual traffic? Blame the intern. ${g}|${d}"
+    echo -e "   ⠸⠁⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ${g}|${d}       ${p}=>${d} Rule hits? Blame the hacker. "
+    echo -e "     ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀      ${g}|${d}       ${o}=>${d} No logs? Blame yourself.  "
+    echo -e "      ⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀ ${g}+-----${d}"
+    echo -e "      ⠹⣿⣿⡇⠈⠻⣿⣿⣿⣿⠀   "
+    echo -e "       ⠈⠻⡇⠀⠀⠈⠙⠿⣿⠀⠀                  ${f}Shout first, check later ...${d}"
+    echo ""
+}
