3845c977f6c1806400d639928e405a92e102319a
ferhs343 <114626248+ferhs343@users.noreply.github.com>
2025-08-11 18:38:07 -0600

    Create Rules.sh

---
+++ Rules.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#All user-defined rules are defined by functions. The syntax is as follows:
+
+#function rule_[protocol]_[rule name]() {}
+
+#Example:
+
+#function rule_http_detection_IoC() {}
