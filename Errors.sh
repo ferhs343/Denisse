28343d37e35a067f00cc771376d6abad2c16ecf1
ferhs343 <114626248+ferhs343@users.noreply.github.com>
2025-08-11 18:32:23 -0600

    Update Errors.sh

--- Errors.sh
+++ Errors.sh
@@ -2,42 +2,56 @@
 
 #This error occurs when a dependency cannot be installed. If this error persists, 
 #it is recommended to perform the installation manually.
-function instalation_error() {
+function instalation_error() 
+{
 
-	echo -e "${red} [x] An unexpected error ocurred"
-	echo -e "     if this persists, it is recommended to perform the installation manually.\n${default}"
+	echo -e "${r} [x] An unexpected error ocurred"
+	echo -e "     if this persists, it is recommended to perform the installation manually.\n${d}"
 }
 
 #This error occurs when the entered pcap file does not exist, verify that the name is correct
-function input_pcap_error() {
+function input_pcap_error() 
+{
 
-	echo -e "${red}\n [x] Error, the file does not exist, try again.${default}"
-}
-
-#This error is caused by the PcapPlusPlus dependency when a pcap is cut. At the time of 
-#Denisse publication, it is unknown if this error occurs on all computers. However, 
-#if this error persists on yours, follow the steps described in the documentation to solve it.
-function pcapplusplus_error() {
-
-	echo -e "${red}\n [x] An error occurred while processing the pcap file."
-	echo -e " If this error persists, follow the steps in the documentation.${default}"
+	echo -e "${r}\n [x] Error, the file does not exist, try again.\n${d}"
 }
 
 #This error occurs when the user provides an unsupported protocol as an argument
 #Supported protocols: [tcp,udp,http,dns,smb2,rpc,dcerpc,ntlm,kerberos,ftp,ssh]
-function input_protocol_error() {
-
-	echo -e "${red}\n ERROR, one of the specified protocols is not valid.\n${default}"
+function input_protocol_error() 
+{
+	array=("$@")
+	echo -e "${r}\n [x] ERROR, one of the specified protocols is not valid. 
+    Supported protocols ==> [${array[*]}] \n${d}"
 }
 
 #This error occurs when the tool is not run as root user
-function root_error() {
+function root_error() 
+{
 
-	echo -e "${red}\n [x] ERROR, to run this you must be root. \n${default}"
+	echo -e "${r}\n [x] ERROR, to run this you must be root. \n${d}"
 }
 
+function option_error() 
+{
 
-function protocols_error() {
+	echo -e "${r}\n [x] ERROR, enter a valid option. \n${d}"
+}
 
-	echo -e "${red}\n [x] An error occurred while finding compatible protocols in the pcap file.\n${default}"
+function protocols_error() 
+{
+
+	echo -e "${r}\n [x] An error occurred while finding compatible protocols in the pcap file.\n${d}"
+}
+
+function process_error()
+{
+
+	echo -e "${r}\n [x] ERROR, the specified number of processes is greater, your CPU has: ${1}.\n${d}"
+}
+
+function limit_error()
+{
+
+	echo -e "${r}\n [x] ERROR, It was not possible to configure the system limits.\n${d}"
 }
